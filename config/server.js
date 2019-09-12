68029040d5046cea53ee17d58d3869a5281f0354
Kallel Roman <kallelr@outlook.com>
2019-09-12 17:00:14 -0300

    configurando consign

--- config/server.js
+++ config/server.js
@@ -1,5 +1,10 @@
 var express = require('express');
+var consign = require('consign');
+
 var app = express(); // a variável var recebe a função express sendo executada e será usada conforme requisitada
 app.set('view engine', 'ejs'); //essa linha define o motor de visualização do express como o ejs
 app.set('views', './app/views'); // define o caminho das views
+
+consign().include('app/routes').into(app);
+
 module.exports = app;
