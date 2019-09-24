6d5f63505314d7ba78ee41d3530c6f5dfa8f33d9
Kallel Roman <kallelr@outlook.com>
2019-09-24 10:21:30 -0300

    Reescrevendo as views | usando express-static

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,5 @@
 module.exports = function(application){
     application.get('/', function(req,res){
-        application.app.controller.home.index(application, req, res);
+        application.app.controllers.home.index(application, req, res);
     });
 }
