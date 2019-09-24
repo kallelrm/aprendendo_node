01e7962b3d43a4911fe0b191e98f1ec5ec7fc551
Kallel Roman <kallelr@outlook.com>
2019-09-24 10:59:54 -0300

    Reaplicando a lógica de backend na home

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,12 @@
 module.exports.index = function(application, res, res){
-    res.render('home/index');
+
+    var connection = application.config.dbConnection();
+    var noticiasModel = new application.app.models.noticiasDAO(connection);
+    
+    noticiasModel.get5UltimasNoticias(function(error, result){
+        console.log(result);
+        
+        res.render('home/index', {noticias: result});
+
+    });
 }
